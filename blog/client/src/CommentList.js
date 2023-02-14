56f6a1cbd5bb531c54d9d74963d4295a38ccf81d
mohamedelserafy <elserafy02@gmail.com>
2023-02-14 17:50:24 +0200

    A Mini-Microservices App

---
+++ blog/client/src/CommentList.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function CommentList({ comments }) {
+  const renderedComments = comments.map((comment) => {
+    let content;
+    if (comment.status === "aproved") {
+      content = comment.content;
+    }
+    if (comment.status === "pending") {
+      content = "this comment is awaiting moderation";
+    }
+    if (comment.status === "rejected") {
+      content = "this comment has been rejected";
+    }
+    return <li key={comment.id}>{content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+}
