6ceb77c6e1a5e07b311213224e462d3252bd14b4
mohamedelserafy <elserafy02@gmail.com>
2023-03-02 10:32:08 +0200

    Adding more services

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -12,17 +12,18 @@ const events = [];
 app.post("/events", async (req, res) => {
   const event = req.body;
   events.push(event);
+  console.log("ended");
   const posts = axios.post("http://posts-clusterip-srv:4000/events", event);
-  // axios.post("http://localhost:4001/events", event);
-  // axios.post("http://localhost:4002/events", event);
-  // axios.post("http://localhost:4003/events", event);
+  const comments = axios.post("http://comments-srv:4001/events", event);
+  const query = axios.post("http://query-srv:4002/events", event);
+  const moderation = axios.post("http://moderation-srv:4003/events", event);
   try {
-    await Promise.all([posts]);
+    await Promise.all([posts, comments, query, moderation]);
   } catch (err) {
     res.send.json({ err: err.message });
     console.log(err.message);
   }
-
+  console.log("ended");
   res.send({ status: "OK" });
 });
 
