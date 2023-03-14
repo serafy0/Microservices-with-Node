ef1683dc822254aba6eaa65c046584de13b4af50
mohamedelserafy <elserafy02@gmail.com>
2023-03-15 01:50:57 +0200

    Creating the User Model

---
+++ ticketing/auth/src/models/user.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const user = mongoose.model("User", userSchema);
