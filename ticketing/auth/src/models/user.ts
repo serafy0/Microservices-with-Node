148e6292c505ddc8662bb0e23e76a5e692b6fcc7
mohamedelserafy <elserafy02@gmail.com>
2023-03-16 00:45:32 +0200

    Mongoose Pre-Save Hooks

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 interface UserAttrs {
   email: string;
   password: string;
@@ -25,6 +26,14 @@ const userSchema = new mongoose.Schema({
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  done();
+});
 
 const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
 export { User };
