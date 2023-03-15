f8f1b5d40e83fb81cb51da3bc78bc5670d527804
mohamedelserafy <elserafy02@gmail.com>
2023-03-15 02:31:51 +0200

    User Creation

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from "express";
 const router = express.Router();
 import { body, validationResult } from "express-validator";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
 import { RequestValidationError } from "../errors/request-validation-error";
+import { User } from "../models/user";
 router.post(
   "/api/users/signup",
   [
@@ -12,17 +12,25 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be 40 and 20 characters"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
-    const { email, password } = req.body;
-    console.log("creating a user...");
-    throw new DatabaseConnectionError();
 
-    res.send({});
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      console.log("Email in use");
+      return res.send({});
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
