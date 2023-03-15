c8c07943e1bd3178221b94a47c959937dee3cb92
mohamedelserafy <elserafy02@gmail.com>
2023-03-15 15:46:45 +0200

    Adding Session Support

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -2,6 +2,7 @@ import express, { NextFunction } from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 import { currentuserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -9,8 +10,16 @@ import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 const app = express();
+//trust traffic from proxy
+app.set("trust proxy", true);
 
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentuserRouter);
 app.use(signinRouter);
