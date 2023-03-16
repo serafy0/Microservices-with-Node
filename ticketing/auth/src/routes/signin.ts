84beb602112429f8af131eadb1a03d1da24b5b5d
mohamedelserafy <elserafy02@gmail.com>
2023-03-16 02:43:54 +0200

    Sign In Logic

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -1,7 +1,11 @@
 import express from "express";
 import { body } from "express-validator";
+import jwt from "jsonwebtoken";
 import { Request, Response } from "express";
+import { User } from "../models/user";
 import { validateRequest } from "../middlewares/validate-request";
+import { BadRequestError } from "../errors/bad-request-error";
+import { Password } from "../services/password";
 const router = express.Router();
 
 router.post(
@@ -15,7 +19,33 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    res.send({});
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials");
+    }
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordsMatch) {
+      throw new BadRequestError("Invalid credentials");
+    }
+    //Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    //Store it on session object
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(201).send(existingUser);
   }
 );
 
