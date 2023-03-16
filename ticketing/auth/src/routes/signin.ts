d06b9486374423b479ed5e9814667d9fa70676eb
mohamedelserafy <elserafy02@gmail.com>
2023-03-16 02:22:57 +0200

    The Signin Flow

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -1,8 +1,25 @@
 import express from "express";
+import { body, validationResult } from "express-validator";
+import { Request, Response } from "express";
+import { RequestValidationError } from "../errors/request-validation-error";
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("hi there!");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("you must supply a password"),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+    res.send({});
+  }
+);
 
 export { router as signinRouter };
