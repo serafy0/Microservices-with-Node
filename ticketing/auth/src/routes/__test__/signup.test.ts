788feb90cce4624a1b8878f77625f622c338bd75
mohamedelserafy <elserafy02@gmail.com>
2023-03-22 05:54:16 +0200

    Testing Invalid Input

--- ticketing/auth/src/routes/__test__/signup.test.ts
+++ ticketing/auth/src/routes/__test__/signup.test.ts
@@ -11,3 +11,34 @@ it("returns a 201 on succe(ssful signup", async () => {
     })
     .expect(201);
 });
+
+it("returns a 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "invalidemail",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with an invalid password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@tes.com",
+      password: "p",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with a missing email or password", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "tes@tes.com" })
+    .expect(400);
+  await request(app)
+    .post("/api/users/signup")
+    .send({ password: "newpassword" })
+    .expect(400);
+});
