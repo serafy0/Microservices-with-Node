db05669089d81bbe21e97de9a112a6ae704d68a0
mohamedelserafy <elserafy02@gmail.com>
2023-03-22 06:30:38 +0200

    Testing Sign Out

---
+++ ticketing/auth/src/routes/__test__/signout.test.ts
@@ -0,0 +1,22 @@
+import request from "supertest";
+
+import { app } from "../../app";
+
+it("clears the cookie after signing out", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "passwrd" })
+    .expect(201);
+
+  await request("api/user/signout")
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "passwrd" })
+    .expect(400);
+
+  const response = await request(app)
+    .post("/api/users/signout")
+    .send({})
+    .expect(200);
+
+  console.log(response.get("Set-Cookie"));
+});
