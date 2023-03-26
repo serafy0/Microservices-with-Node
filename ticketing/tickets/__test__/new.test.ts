f4b24062c809dbe24ee885c304c3aa9890d20652
mohamedelserafy <elserafy02@gmail.com>
2023-03-26 13:39:00 +0200

    Test-First Approach

---
+++ ticketing/tickets/__test__/new.test.ts
@@ -0,0 +1,12 @@
+import request from "supertest";
+import { app } from "../src/app";
+
+it("has a route handler listening to /api/tickets for post requests", async () => {});
+
+it("can only be accessed if user is signed in", async () => {});
+
+it("it returns an error if invalid title is provided", async () => {});
+
+it("it return an error if invalid price is provided", async () => {});
+
+it("creates a ticket with valid inputs", async () => {});
