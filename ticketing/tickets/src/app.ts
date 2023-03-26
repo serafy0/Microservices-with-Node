1ec3693c9a8c06cb2c6cc0cff3bc636260cc14a0
mohamedelserafy <elserafy02@gmail.com>
2023-03-26 12:53:21 +0200

    Project Setup

---
+++ ticketing/tickets/src/app.ts
@@ -0,0 +1,25 @@
+import express, { NextFunction } from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError } from "@ticketing-s/common";
+
+const app = express();
+//trust traffic from proxy
+app.set("trust proxy", true);
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
