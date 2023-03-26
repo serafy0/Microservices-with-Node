f9875db759ebd5ff998a5d9f46363cc21e3c50a4
mohamedelserafy <elserafy02@gmail.com>
2023-03-26 13:45:37 +0200

    Creating the Router

--- ticketing/tickets/src/app.ts
+++ ticketing/tickets/src/app.ts
@@ -4,6 +4,8 @@ import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError } from "@ticketing-s/common";
 
+import { createTicketRouter } from "../routes/new";
+
 const app = express();
 //trust traffic from proxy
 app.set("trust proxy", true);
@@ -15,6 +17,8 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
+
+app.use(createTicketRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
