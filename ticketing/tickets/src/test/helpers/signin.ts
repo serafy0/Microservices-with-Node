296bc9fd9a0e885558b714425b02e59e5d17b7d9
mohamedelserafy <elserafy02@gmail.com>
2023-03-26 14:35:07 +0200

    Faking Auth During Tests & Building a Session

--- ticketing/tickets/src/test/helpers/signin.ts
+++ ticketing/tickets/src/test/helpers/signin.ts
@@ -1,20 +1,23 @@
 import { app } from "../../app";
 import request from "supertest";
+import jwt from "jsonwebtoken";
 
-const signin = async () => {
-  const email = "test@test.com";
-  const password = "password";
-
-  const response = await request(app)
-    .post("/api/users/signup")
-    .send({
-      email,
-      password,
-    })
-    .expect(201);
-
-  const cookie = response.get("Set-Cookie");
-  return cookie;
+const signin = (): string[] => {
+  //Build a JWT payload. {id, email}
+  const payload = {
+    id: "3d32drds4r",
+    email: "test@test1.com",
+  };
+  //Create the JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  //Build session Object
+  const session = { jwt: token };
+  //Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+  //Take JSON and enconde it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+  //return a string thats the cookie with the encoded data
+  return [`session=${base64}`];
 };
 
 export { signin };
