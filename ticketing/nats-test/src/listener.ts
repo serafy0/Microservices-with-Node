181fceed8f02d516df8760bc49f0b75d96b1d763
mohamedelserafy <elserafy02@gmail.com>
2023-03-29 15:46:34 +0200

    Manual Ack Mode

--- ticketing/nats-test/src/listener.ts
+++ ticketing/nats-test/src/listener.ts
@@ -7,15 +7,18 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 });
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
+  const options = stan.subscriptionOptions().setManualAckMode(true);
   const subscription = stan.subscribe(
     "ticket:created",
-    "orders-service-queue-group"
+    "orders-service-queue-group",
+    options
   );
   subscription.on("message", (msg: Message) => {
     const data = msg.getData();
 
     if (typeof data === "string") {
-      console.log(`received event ${msg.getSequence()}, with data: ${data}`);
+      console.log(`received event #${msg.getSequence()}, with data: ${data}`);
     }
+    msg.ack();
   });
 });
