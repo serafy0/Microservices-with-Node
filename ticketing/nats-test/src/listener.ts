a4dfb7593f628a756045e8f979e3d4efdb2b2b28
mohamedelserafy <elserafy02@gmail.com>
2023-03-31 14:56:11 +0200

    Quick Refactor

--- ticketing/nats-test/src/listener.ts
+++ ticketing/nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message, Stan } from "node-nats-streaming";
+import nats from "node-nats-streaming";
 import { randomBytes } from "crypto";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 console.clear();
 
 const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
@@ -17,60 +18,3 @@ stan.on("connect", () => {
 
 process.on("SIGINT", () => stan.close());
 process.on("SIGTERM", () => stan.close());
-
-abstract class Listener {
-  private client: Stan;
-
-  abstract subject: string;
-  abstract queueGroupName: string;
-
-  private ackWait = 5 * 1000;
-
-  constructor(client: Stan) {
-    this.client = client;
-  }
-
-  subscriptionOptions() {
-    return this.client
-      .subscriptionOptions()
-      .setDeliverAllAvailable()
-      .setManualAckMode(true)
-      .setAckWait(this.ackWait)
-      .setDurableName(this.queueGroupName);
-  }
-
-  listen() {
-    const subscription = this.client.subscribe(
-      this.subject,
-      this.queueGroupName,
-      this.subscriptionOptions()
-    );
-
-    subscription.on("message", (msg: Message) => {
-      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
-
-      const parseData = this.parseMessage(msg);
-      this.onMessage(parseData, msg);
-    });
-  }
-
-  parseMessage(msg: Message) {
-    const data = msg.getData();
-
-    return typeof data === "string"
-      ? JSON.parse(data)
-      : JSON.parse(data.toString("utf8"));
-  }
-
-  abstract onMessage(data: any, msg: Message): void;
-}
-
-class TicketCreatedListener extends Listener {
-  subject = "ticket:created";
-  queueGroupName = "payments-service";
-
-  onMessage(data: any, msg: Message) {
-    console.log("Event Data!", data);
-    msg.ack();
-  }
-}
