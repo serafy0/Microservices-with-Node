6de709ac048030ed803541607db8341e8689bee3
mohamedelserafy <elserafy02@gmail.com>
2023-04-13 01:41:17 +0200

    Singletion Implementation

--- ticketing/tickets/nats-wrapper.ts
+++ ticketing/tickets/nats-wrapper.ts
@@ -1,4 +1,20 @@
 import nats, { Stan } from "node-nats-streaming";
 
-class NatsWrapper {}
+class NatsWrapper {
+  private _client?: Stan;
+
+  connect(clusterId: string, cleintId: string, url: string) {
+    this._client = nats.connect(clusterId, cleintId, { url });
+    return new Promise((resolve, reject) => {
+      this._client!.on("connect", () => {
+        console.log("connected to nats");
+        resolve("connected");
+      });
+
+      this._client!.on("error", (err) => {
+        reject(err);
+      });
+    });
+  }
+}
 export const natsWrapper = new NatsWrapper();
--- ticketing/tickets/src/index.ts
+++ ticketing/tickets/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { app } from "./app";
+import { natsWrapper } from "../nats-wrapper";
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
@@ -8,6 +9,7 @@ const start = async () => {
     throw new Error("MONGO_URI must be defined");
   }
   try {
+    await natsWrapper.connect("ticketing", "laskjf", "http://nats-srv:4222");
     await mongoose.connect(process.env.MONGO_URI);
     console.log("connected to mongoDB");
   } catch (err) {
