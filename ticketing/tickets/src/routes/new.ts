f9da03f2c26e623b029c573cbc08eda0cca68c9b
mohamedelserafy <elserafy02@gmail.com>
2023-04-13 00:35:41 +0200

    More on Publishing

--- ticketing/tickets/src/routes/new.ts
+++ ticketing/tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import { requireAuth, validateRequest } from "@ticketing-s/common";
 import { body } from "express-validator";
 import { Ticket } from "../models/ticket";
+import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
+
 const router = express.Router();
 
 router.post(
@@ -19,6 +21,12 @@ router.post(
     const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
 
     await ticket.save();
+    await new TicketCreatedPublisher(client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
     res.status(201).send(ticket);
   }
 );
