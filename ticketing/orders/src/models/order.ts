f6f9465f4fb4ccaebe74a658d3b4488271f5a959
mohamedelserafy <elserafy02@gmail.com>
2023-04-28 23:10:34 +0300

    Defining the Ticket Model

--- ticketing/orders/src/models/order.ts
+++ ticketing/orders/src/models/order.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { OrderStatus } from "@ticketing-s/common";
+import { TicketDoc } from "./ticket";
 interface OrderAttrs {
   userId: string;
   status: OrderStatus;
