f03f488c46b20a17782a1dc1741a235991a3acf7
mohamedelserafy <elserafy02@gmail.com>
2023-04-29 00:37:42 +0300

    Test Suite Setup

---
+++ ticketing/orders/src/test/helpers/signin.ts
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+const signin = (): string[] => {
+  //Build a JWT payload. {id, email}
+
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: "test@test1.com",
+  };
+  //Create the JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  //Build session Object
+  const session = { jwt: token };
+  //Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+  //Take JSON and enconde it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+  //return a string thats the cookie with the encoded data
+  return [`session=${base64}`];
+};
+
+export { signin };
