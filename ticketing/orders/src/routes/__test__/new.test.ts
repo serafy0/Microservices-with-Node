0f34e46f1933221b42f25a1068389d7e6bf4afc6
mohamedelserafy <elserafy02@gmail.com>
2023-05-03 02:19:20 +0300

    Asserting Reserved Tickets

--- ticketing/orders/src/routes/__test__/new.test.ts
+++ ticketing/orders/src/routes/__test__/new.test.ts
@@ -2,6 +2,9 @@ import request from "supertest";
 import { app } from "../../app";
 import mongoose from "mongoose";
 import { signin } from "../../test/helpers/signin";
+import { Ticket } from "../../models/ticket";
+import { Order } from "../../models/order";
+import { OrderStatus } from "@ticketing-s/common";
 
 it("returns an error if the ticket does not exist", async () => {
   const ticketId = new mongoose.Types.ObjectId();
@@ -13,5 +16,26 @@ it("returns an error if the ticket does not exist", async () => {
     .expect(404);
 });
 
-it("returns an error if the ticket is already reserved", async () => {});
+it("returns an error if the ticket is already reserved", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+
+  await ticket.save();
+  const order = Order.build({
+    ticket,
+    userId: "hugfyddsjk",
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+
+  await order.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", signin())
+    .send({ ticketId: ticket.id })
+    .expect(400);
+});
 it("reserves a ticket", async () => {});
