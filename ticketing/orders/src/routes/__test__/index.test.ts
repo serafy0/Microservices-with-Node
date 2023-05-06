ab3a2db80c077614355bacbf1a873e8b7deea6e1
mohamedelserafy <elserafy02@gmail.com>
2023-05-06 18:27:19 +0300

    typo

--- ticketing/orders/src/routes/__test__/index.test.ts
+++ ticketing/orders/src/routes/__test__/index.test.ts
@@ -47,7 +47,7 @@ it("fetches orders for a praticular user", async () => {
   //make sure we only got the orders for User#2
   expect(response.body.length).toEqual(2);
   expect(response.body[0].id).toEqual(orderOne.id);
-  expect(response.body[0].id).toEqual(orderTwo.id);
+  expect(response.body[1].id).toEqual(orderTwo.id);
   expect(response.body[0].ticket.id).toEqual(ticketTwo.id);
-  expect(response.body[0].ticket.id).toEqual(ticketThree.id);
+  expect(response.body[1].ticket.id).toEqual(ticketThree.id);
 });
