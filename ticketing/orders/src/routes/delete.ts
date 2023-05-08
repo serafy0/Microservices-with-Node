3a47c4d16a6a8f92e123fb84af07dbe2a4c9ce41
mohamedelserafy <elserafy02@gmail.com>
2023-05-08 04:47:18 +0300

    Cancelling an Order

--- ticketing/orders/src/routes/delete.ts
+++ ticketing/orders/src/routes/delete.ts
@@ -1,9 +1,29 @@
 import express, { Request, Response } from "express";
+import { Order } from "../models/order";
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from "@ticketing-s/common";
 
 const router = express.Router();
 
-router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    await order.save();
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
