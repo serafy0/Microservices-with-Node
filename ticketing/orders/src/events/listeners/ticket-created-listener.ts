de9bc74b79d6534d8d2172dc72c70f37fb26ed91
mohamedelserafy <elserafy02@gmail.com>
2023-05-10 15:25:31 +0300

    Simple onMessage Implementation

--- ticketing/orders/src/events/listeners/ticket-created-listener.ts
+++ ticketing/orders/src/events/listeners/ticket-created-listener.ts
@@ -1,9 +1,18 @@
 import { Listener, Subjects, TicketCreatedEvent } from "@ticketing-s/common";
 import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
+import { Ticket } from "../../models/ticket";
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = queueGroupName;
-  onMessage(data: TicketCreatedEvent["data"], msg: Message): void {}
+  async onMessage(
+    data: TicketCreatedEvent["data"],
+    msg: Message
+  ): Promise<void> {
+    const { title, price } = data;
+    const ticket = Ticket.build({ title, price });
+    await ticket.save();
+    msg.ack();
+  }
 }
