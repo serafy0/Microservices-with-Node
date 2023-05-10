0755858018e8d50d82db0561d8642ce088ee3fec
mohamedelserafy <elserafy02@gmail.com>
2023-05-10 15:37:04 +0300

    Ticket Updated Listener Implementation

---
+++ ticketing/orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,25 @@
+import { Listener, Subjects, TicketUpdatedEvent } from "@ticketing-s/common";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(
+    data: TicketUpdatedEvent["data"],
+    msg: Message
+  ): Promise<void> {
+    const ticket = await Ticket.findById(data.id);
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+    const { title, price } = data;
+    ticket.set({ title, price });
+    await ticket.save();
+
+    msg.ack();
+  }
+}
