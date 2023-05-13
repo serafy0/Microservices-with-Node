90c3bff7ab764a81b49faf62fdeefee10fe48b0f
mohamedelserafy <elserafy02@gmail.com>
2023-05-13 16:36:51 +0300

    Versioning With Update-If-Current

--- ticketing/orders/src/models/ticket.ts
+++ ticketing/orders/src/models/ticket.ts
@@ -1,6 +1,7 @@
 import { OrderStatus } from "@ticketing-s/common";
 import mongoose from "mongoose";
 import { Order } from "./order";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 
 interface TicketAttrs {
   id?: string;
@@ -44,12 +45,8 @@ const ticketSchema = new mongoose.Schema(
 );
 
 ticketSchema.set("versionKey", "version");
-// ticketSchema.plugin(updateIfCurrentPlugin);
+ticketSchema.plugin(updateIfCurrentPlugin);
 
-ticketSchema.pre("save", function (done) {
-  this.$where = { version: this.get("version") - 1 };
-  done();
-});
 ticketSchema.statics.findByEvent = async (event: {
   id: string;
   version: number;
