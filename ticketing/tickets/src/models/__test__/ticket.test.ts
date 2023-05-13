ffdb985f7e97a2d821e379943d90bee008a6de82
mohamedelserafy <elserafy02@gmail.com>
2023-05-13 10:29:01 +0300

    One More Test

--- ticketing/tickets/src/models/__test__/ticket.test.ts
+++ ticketing/tickets/src/models/__test__/ticket.test.ts
@@ -25,3 +25,18 @@ it("implements optimistic concurrency control", async () => {
   }
   throw new Error("Test should have thrown");
 });
+
+it("increments the version number on multiple saves", async () => {
+  const ticket = Ticket.build({
+    title: "concerts",
+    price: 20,
+    userId: "123",
+  });
+
+  await ticket.save();
+  expect(ticket.version).toEqual(0);
+  await ticket.save();
+  expect(ticket.version).toEqual(1);
+  await ticket.save();
+  expect(ticket.version).toEqual(2);
+});
