b5692005a236a56e52e182f74a2325c739399ce3
mohamedelserafy <elserafy02@gmail.com>
2023-05-13 16:45:05 +0300

    Testing Listeners

---
+++ ticketing/orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -0,0 +1,16 @@
+const setup = async () => {
+  //create an instance of the listener
+  //create a fake data event
+  //create a fake message object
+};
+
+it("creates and saves a ticket", async () => {
+  //call the onMessage function with the data object + message object
+  //write assertions to make sure a ticket was created
+});
+
+it("acknowledges the message", async () => {
+  //call the onMessage function with the data object + message object
+  //write assertions to make sure a ticket was created
+  //write assertions to make sure the ack function is called
+});
