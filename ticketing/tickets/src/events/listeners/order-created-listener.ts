3d529e27b9c842767cab97c87fa7ea76cfd25aed
mohamedelserafy <elserafy02@gmail.com>
2023-05-14 08:39:14 +0300

    Reserving a Ticket

--- ticketing/tickets/src/events/listeners/order-created-listener.ts
+++ ticketing/tickets/src/events/listeners/order-created-listener.ts
@@ -1,8 +1,22 @@
 import { Listener, OrderCreatedEvent, Subjects } from "@ticketing-s/common";
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName: string = queueGroupName;
-  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    //find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+    //if no ticket, throw error
+    if (!ticket) {
+      throw new Error("ticket not found");
+    }
+    //mark the ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: data.id });
+    //save the ticket
+    await ticket.save();
+    //ack the message
+    msg.ack();
+  }
 }
