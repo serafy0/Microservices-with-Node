3cf49858457e0a07178c43c49f19b4b428dd448d
mohamedelserafy <elserafy02@gmail.com>
2023-05-14 08:25:49 +0300

    Building the Listener

---
+++ ticketing/tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,8 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@ticketing-s/common";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from "node-nats-streaming";
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName: string = queueGroupName;
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+}
