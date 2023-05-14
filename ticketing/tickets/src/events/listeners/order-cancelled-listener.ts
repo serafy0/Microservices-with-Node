7b97f8bebbed5db74cd3c6622a19181be4d8957f
mohamedelserafy <elserafy02@gmail.com>
2023-05-14 10:01:53 +0300

    Order Cancelled Listener

---
+++ ticketing/tickets/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,32 @@
+import { Listener, OrderCancelledEvent, Subjects } from "@ticketing-s/common";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
+
+  queueGroupName: string = queueGroupName;
+
+  async onMessage(
+    data: OrderCancelledEvent["data"],
+    msg: Message
+  ): Promise<void> {
+    const ticket = await Ticket.findById(data.ticket.id);
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+    ticket.set({ orderId: undefined });
+    await ticket.save();
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      orderId: ticket.orderId,
+      userId: ticket.userId,
+      price: ticket.price,
+      version: ticket.version,
+      title: ticket.title,
+    });
+    msg.ack();
+  }
+}
