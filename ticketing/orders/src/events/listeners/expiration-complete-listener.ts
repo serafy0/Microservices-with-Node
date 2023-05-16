6cbe0fb787f5748eac1519beadae52aff62fb68d
mohamedelserafy <elserafy02@gmail.com>
2023-05-16 09:33:42 +0300

    Handling an Expiration Event

---
+++ ticketing/orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,26 @@
+import {
+  Listener,
+  ExpirationCompleteEvent,
+  Subjects,
+  OrderStatus,
+} from "@ticketing-s/common";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+  queueGroupName = queueGroupName;
+  async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
+    const order = await Order.findById(data.orderId);
+
+    if (!order) {
+      throw new Error("order not found");
+    }
+
+    order.set({
+      status: OrderStatus.Cancelled,
+      ticket: null,
+    });
+  }
+}
