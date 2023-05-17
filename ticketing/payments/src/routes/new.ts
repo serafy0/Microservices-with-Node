3257e26b505bcd3bc35bef71fdc17404166eaf7d
mohamedelserafy <elserafy02@gmail.com>
2023-05-17 13:56:55 +0300

    Validating Order Payment

--- ticketing/payments/src/routes/new.ts
+++ ticketing/payments/src/routes/new.ts
@@ -1,6 +1,14 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
-import { requireAuth, validateRequest } from "@ticketing-s/common";
+import {
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from "@ticketing-s/common";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
@@ -9,8 +17,20 @@ router.post(
   requireAuth,
   [body("token").not().isEmpty(), body("orderId").not().isEmpty()],
   validateRequest,
-  (req: Request, res: Response) => {
-    res.send({ sucess: true });
+  async (req: Request, res: Response) => {
+    const { orderId } = req.body;
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError("can't pay for a cancelled order");
+    }
+    res.send({ success: true });
   }
 );
 
