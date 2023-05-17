515ff04e1738f44fb0977fd040c89d41ce92e285
mohamedelserafy <elserafy02@gmail.com>
2023-05-17 09:07:21 +0300

    Update-If-Current

--- ticketing/payments/src/models/order.ts
+++ ticketing/payments/src/models/order.ts
@@ -1,5 +1,6 @@
 import { OrderStatus } from "@ticketing-s/common";
 import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 
 interface OrderAttrs {
   id: string;
@@ -40,6 +41,9 @@ const orderSchema = new mongoose.Schema(
     },
   }
 );
+
+orderSchema.set("versionKey", "version");
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order({
